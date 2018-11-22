623dd923a5e3e1088bab30b4abf97c445845dd77
hectopk <hectopk@gmail.com>
2018-11-23 00:29:33 +0300

    v 0.1.4

--- russianseasons/admin/urls.py
+++ russianseasons/admin/urls.py
@@ -7,12 +7,12 @@ from django.views.generic import RedirectView
 from russianseasons.admin import views
 
 urlpatterns = [
-	path('', views.admin_page, name='admin_url'),
+	# path('', views.admin_page, name='admin_url'),
 	path('login', views.LoginPage.as_view(), name='login_url'),
 	path('logout', views.logout_page, name='logout_url'),
 	path('shop', views.ShopPage.as_view(), name='admin_shop_url'),
 	path('orders', views.OrdersPage.as_view(), name='admin_orders_url'),
-	url('^shop/item/new$', views.NewItem.as_view(), name='new_item_url'),
+	path('shop/item/new', views.NewItem.as_view(), name='new_item_url'),
 	path('shop/colors/new', views.NewColor.as_view(), name='new_color_url'),
 	path('shop/sizes/new', views.NewSize.as_view(), name='new_size_url'),
 	path('shop/item/edit/<int:id>', views.EditItem.as_view(), name='edit_item_url'),
--- russianseasons/admin/views.py
+++ russianseasons/admin/views.py
@@ -6,19 +6,94 @@ from russianseasons.forms import *
 from russianseasons.models import *
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
+from django.http import Http404
 from russianseasons.admin.forms import *
 from django.contrib.auth import authenticate, login, logout
 
 sizes = ['s', 'm', 'l', 'xl']
 colors = ['black', 'grey', 'white']
 
-class BaseAdminView(View):
-	admin = True
-	context = {}
-	# def as_view(self, *args, **kwargs):
-	# 	# super(CheckboxWidget, self).render(*args, **kwargs)
-	# 	page = super(BaseAdminView, self).as_view( *args, **kwargs)
-	# 	return page
+from django.utils.decorators import classonlymethod
+from functools import update_wrapper
+
+class BaseAdminView:
+
+	"""
+	Intentionally simple parent class for all views. Only implements
+	dispatch-by-method and simple sanity checking.
+	"""
+
+	http_method_names = ['get', 'post', 'put', 'patch', 'delete', 'head', 'options', 'trace']
+	def __init__(self, **kwargs):
+		"""
+		Constructor. Called in the URLconf; can contain helpful extra
+		keyword arguments, and other things.
+		"""
+		# Go through keyword arguments, and either save their values to our
+		# instance, or raise an error.
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+
+	@classonlymethod
+	def as_view(cls, **initkwargs):
+		"""Main entry point for a request-response process."""
+		for key in initkwargs:
+			if key in cls.http_method_names:
+				raise TypeError("You tried to pass in the %s method name as a "
+								"keyword argument to %s(). Don't do that."
+								% (key, cls.__name__))
+			if not hasattr(cls, key):
+				raise TypeError("%s() received an invalid keyword %r. as_view "
+								"only accepts arguments that are already "
+								"attributes of the class." % (cls.__name__, key))
+
+		def view(request, *args, **kwargs):
+			if not request.user.is_authenticated:
+				raise Http404
+			self = cls(**initkwargs)
+			if hasattr(self, 'get') and not hasattr(self, 'head'):
+				self.head = self.get
+			self.request = request
+			self.args = args
+			self.kwargs = kwargs
+			return self.dispatch(request, *args, **kwargs)
+		view.view_class = cls
+		view.view_initkwargs = initkwargs
+
+		# take name and docstring from class
+		update_wrapper(view, cls, updated=())
+
+		# and possible attributes set by decorators
+		# like csrf_exempt from dispatch
+		update_wrapper(view, cls.dispatch, assigned=())
+		return view
+
+	def dispatch(self, request, *args, **kwargs):
+		# Try to dispatch to the right method; if a method doesn't exist,
+		# defer to the error handler. Also defer to the error handler if the
+		# request method isn't on the approved list.
+		if request.method.lower() in self.http_method_names:
+			handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+		else:
+			handler = self.http_method_not_allowed
+		return handler(request, *args, **kwargs)
+
+	def http_method_not_allowed(self, request, *args, **kwargs):
+		logger.warning(
+			'Method Not Allowed (%s): %s', request.method, request.path,
+			extra={'status_code': 405, 'request': request}
+		)
+		return HttpResponseNotAllowed(self._allowed_methods())
+
+	def options(self, request, *args, **kwargs):
+		"""Handle responding to requests for the OPTIONS HTTP verb."""
+		response = HttpResponse()
+		response['Allow'] = ', '.join(self._allowed_methods())
+		response['Content-Length'] = '0'
+		return response
+
+	def _allowed_methods(self):
+		return [m.upper() for m in self.http_method_names if hasattr(self, m)]
 
 def admin_page(request):
 	template_name = 'admin/admin_page.html'
@@ -90,6 +165,8 @@ class LoginPage(View):
 	template_name = 'admin/login_page.html'
 	context = {'login_form': LoginForm()}
 	def get(self, request):
+		if request.user.is_authenticated:
+			return HttpResponseRedirect(reverse('admin_shop_url'))
 		return render(request, self.template_name, self.context)
 
 	def post(self, request):
