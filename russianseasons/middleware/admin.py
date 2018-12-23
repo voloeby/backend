354bb1b97018c6bcf655d4c85c79c38432fe1c70
hectopk <hectopk@gmail.com>
2018-12-23 15:48:27 +0300

    0.2.46

--- russianseasons/middleware/admin.py
+++ russianseasons/middleware/admin.py
@@ -4,8 +4,8 @@ from russianseasons.models import Profile
 
 class CheckUserActivityMiddleware(MiddlewareMixin):
 	def process_request(self, request):
-		if request.user.is_authenticated:
-			request.user.profile.last_activity = timezone.now()
-			request.user.save()
-		print(request.user.profile.last_activity)
+		if request.user:
+			if request.user.is_authenticated:
+				request.user.profile.last_activity = timezone.now()
+				request.user.save()
 		return None
