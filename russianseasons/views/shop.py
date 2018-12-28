b5a746a0c597cf5839d88c575b1c3d390391b595
hectopk <hectopk@gmail.com>
2018-12-28 19:55:44 +0300

    0.3.10

--- russianseasons/views/shop.py
+++ russianseasons/views/shop.py
@@ -7,37 +7,38 @@ from django.http import HttpResponse
 
 
 def shop_page(request):
-
-    context = {}
-    template_name = 'shop.html'
-    context['items'] = ItemPrototype.objects.all()
-    res = render(request, template_name, context)
-    return res
+	context = {}
+	template_name = 'shop.html'
+	context['items'] = ItemPrototype.objects.all()
+	res = render(request, template_name, context)
+	return res
 
 
 class ItemView(View):
-    def get(self, request, id):
-        context = {}
-        template_name = 'item_page.html'
-        item = get_object_or_404(ItemPrototype, id=id)
-        if item.images.count() > 0:
-            item.main_image = item.images.all()[0]
-            item.small_images = item.images.all()[1:]
-        else:
-            pass
-        context['item'] = item
-        return render(request, template_name, context)
+	def get(self, request, id):
+		context = {}
+		template_name = 'item_page.html'
+		item = get_object_or_404(ItemPrototype, id=id)
+		if not item.show:
+			raise Http404
+		if item.images.count() > 0:
+			item.main_image = item.images.all()[0]
+			item.small_images = item.images.all()[1:]
+		else:
+			pass
+		context['item'] = item
+		return render(request, template_name, context)
 
-    def post(self, request, id):
-        item_prot = get_object_or_404(ItemPrototype, id=id)
-        color, created = Color.objects.get_or_create(name=request.POST['color'])
-        size, created = Size.objects.get_or_create(name=request.POST['size'])
-        item, created = Item.objects.get_or_create(color=color, size=size, prototype=item_prot)
-        order = Order()
-        order.save()
-        order.is_preorder = True
-        order.email = request.POST['email']
-        order.city = request.POST['city']
-        order.items.add(item)
-        order.save()
-        return HttpResponse('ok')
+	def post(self, request, id):
+		item_prot = get_object_or_404(ItemPrototype, id=id)
+		color, created = Color.objects.get_or_create(name=request.POST['color'])
+		size, created = Size.objects.get_or_create(name=request.POST['size'])
+		item, created = Item.objects.get_or_create(color=color, size=size, prototype=item_prot)
+		order = Order()
+		order.save()
+		order.is_preorder = True
+		order.email = request.POST['email']
+		order.city = request.POST['city']
+		order.items.add(item)
+		order.save()
+		return HttpResponse('ok')
